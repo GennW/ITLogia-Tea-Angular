2e5eca4ca263ddbf87f86f125230596e62b84d1b
GennW <778855@mail.ru>
2024-05-20 17:14:52 +0300

    добавлен редирект на главную если неправильный роут

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: '/main', pathMatch: 'full' }, // Маршрут по умолчанию
+  { path: '**', redirectTo: '/main' }
 ];
 
 @NgModule({
